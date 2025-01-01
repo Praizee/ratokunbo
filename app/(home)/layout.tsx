32d0c2e501296db7d24a9631da32624403524781
Stephen Adeniji <praiseadeniji2017@gmail.com>
2025-01-01 22:17:20 +0100

    layout

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "../_components";
+
+export default function BaseLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
---
+++ app/(home)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen border-4 m-1 border-black flex items-center justify-center">
+      <h1 className="text-4xl font-semibold">Homepage</h1>
+    </div>
+  );
+}
+
